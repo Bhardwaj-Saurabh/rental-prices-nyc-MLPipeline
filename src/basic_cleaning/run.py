b73d9af5ca2608929e1989883a9efe6bd2eb026a
saurabh <aryan.saurabhbhardwaj@gmail.com>
2023-11-01 17:28:56 +0000

    Get data dependencies fixed

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,8 @@ Performs basic cleaning on the data and save the results in Weights & Biases
 import argparse
 import logging
 import wandb
+import pandas as pd
+
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -18,11 +20,18 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    
+    logger.info("Downloading input artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    dataset = pd.read_csv(artifact_local_path)  
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # Perform basic cleaning    
+    logger.info("Removing outliers using min and max prices")
+    id_ = dataset["price"].between(args.min_price, args.max_price)  
+    dataset = dataset[id_].copy()
+
+    
+
 
 
 if __name__ == "__main__":
@@ -31,26 +40,46 @@ if __name__ == "__main__":
 
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact", 
+        type=str
+        help="Name of the preprocessing artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact", 
+        type=str,
+        help="Name of the cleaning artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact", 
+        type=str,
+        help="Name of the cleaning sample",
         required=True
     )
 
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Description of the preprocesses data sample",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Min price for the prediction",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="Max price for the prediction",
+        required=True
+    )
 
     args = parser.parse_args()
 
