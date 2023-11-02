913c19368d466ab74ed457c3c1c0e09b792a432d
saurabh <aryan.saurabhbhardwaj@gmail.com>
2023-11-02 01:03:50 +0000

    Data split completed

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -8,7 +8,6 @@ import wandb
 import pandas as pd
 
 
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
@@ -30,18 +29,36 @@ def go(args):
     id_ = dataset["price"].between(args.min_price, args.max_price)  
     dataset = dataset[id_].copy()
 
+    # Restrict long and lat between -74.25, -73.50
+    idx_ = dataset['longitude'].between(-74.25, -73.50) & dataset['latitude'].between(40.5, 41.2)
+    dataset = dataset[idx_].copy()
+
+    # parse last_review to datetime
+    dataset['last_review'] = pd.to_datetime(dataset['last_review'])
+
+    # Save CSV file
+    logger.info("Saving the results to CSV")
+    dataset.to_csv(args.output_artifact, index=False)
+
+    # Prepare artifact
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+
+    logger.info("Clean sample is prepared")
+    artifact.add_file(args.output_artifact)
+    run.log_artifact(artifact)
+
     
-
-
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="this steps cleans the data")
 
-
     parser.add_argument(
         "--input_artifact", 
-        type=str
+        type=str,
         help="Name of the preprocessing artifact",
         required=True
     )
@@ -54,7 +71,7 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--output_artifact", 
+        "--output_type", 
         type=str,
         help="Name of the cleaning sample",
         required=True
